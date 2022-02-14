e8068c9d55105b5a0c78e21e6e64678e1d82a6ee
Howdy <howdy1078@gmail.com>
2022-02-14 13:50:54 -0700

    Console log error if loading component has error

--- src/components/loading-component.js
+++ src/components/loading-component.js
@@ -1,5 +1,12 @@
 import CircularProgress from "@mui/material/CircularProgress";
+import { useEffect } from "react";
 
 export const LoadingComponent = ({ loading, error, children }) => {
+  useEffect(() => {
+    if (error && !loading) {
+      console.error(error);
+    }
+  }, [error]);
+
   return loading ? <CircularProgress /> : error ? "error" : children;
 };
