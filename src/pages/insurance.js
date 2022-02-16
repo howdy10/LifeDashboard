49250118be85fe44868a5d70f968ae2498dc1bba
Howdy <howdy1078@gmail.com>
2022-02-16 13:59:31 -0700

    code clean up

--- src/pages/insurance.js
+++ src/pages/insurance.js
@@ -1,23 +1,17 @@
 import Head from "next/head";
 import { Box, Container } from "@mui/material";
-import { CarloanBoard } from "src/components/carloan/carloan-board";
 import { DashboardLayout } from "../components/dashboard-layout";
-import { customers } from "../__mocks__/customers";
 import { InsuranceClaims } from "src/components/insurance/insurance-claims";
 import { ref, getDatabase } from "firebase/database";
-import { useObject, useObjectVal } from "react-firebase-hooks/database";
+import { useObjectVal } from "react-firebase-hooks/database";
 import { firebase } from "../firebase/clientApp";
 import AdapterDateFns from "@mui/lab/AdapterDateFns";
 import LocalizationProvider from "@mui/lab/LocalizationProvider";
-import { getAuth } from "firebase/auth";
 import { InsuranceBoard } from "../components/insurance/insurance-board";
 import { InsuranceUrl } from "../firebase/databaseLinks";
 
 export const Insurance = () => {
   const database = getDatabase(firebase);
-
-  const auth = getAuth();
-  const user = auth.currentUser;
 
   const [snapshot, loading, error] = useObjectVal(ref(database, InsuranceUrl()));
 
