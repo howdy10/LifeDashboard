dee732c26bc37c43716bdddeca14d3d86f949408
Howdy <howdy1078@gmail.com>
2022-05-11 09:47:29 -0700

    Created reminder hooks

---
+++ src/hooks/reminders.js
@@ -0,0 +1,32 @@
+import { useState, useMemo, useEffect, useContext } from "react";
+import { ref, getDatabase } from "firebase/database";
+import { useObjectVal } from "react-firebase-hooks/database";
+import { firebase } from "../firebase/clientApp";
+import { RemindersUrl } from "src/firebase/databaseConstants";
+import AppContext from "src/context/AppContext";
+
+export const GetReminders = () => {
+  const database = getDatabase(firebase);
+  const value = useContext(AppContext);
+
+  const [total, setTotal] = useState({
+    reminders: [],
+  });
+
+  const [list, loading, error] = useObjectVal(
+    ref(database, value.state.familyIdBaseUrl + RemindersUrl)
+  );
+
+  useEffect(() => {
+    // if (payChecks) {
+    //   Object.keys(payChecks).map((key, index) => (paidThisMonth += payChecks[key].amount));
+    // }
+    setTotal({
+      ...total,
+      reminders: list,
+    });
+  }, [list]);
+
+  const resArray = [total, loading, error];
+  return useMemo(() => resArray, resArray);
+};
