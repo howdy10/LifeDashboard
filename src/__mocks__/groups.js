f6a3b76bf5c5a5720ef74651fdfe78216490a7a3
Howdy <howdy1078@gmail.com>
2022-08-26 23:43:34 -0700

    mock update

---
+++ src/__mocks__/groups.js
@@ -0,0 +1,20 @@
+export const categoryGroup = [
+  {
+    name: "transportation",
+    subCategory: [
+      {
+        name: "Gas",
+        amount: 350,
+      },
+    ],
+  },
+  {
+    name: "food",
+    subCategory: [
+      {
+        name: "Treat-yourself",
+        amount: 100,
+      },
+    ],
+  },
+];
