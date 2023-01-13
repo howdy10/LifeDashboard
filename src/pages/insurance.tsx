1f182114f5f9ec594892822ddbf2004a6da9831d
Howdy <howdy1078@gmail.com>
2023-01-12 20:15:51 -0700

    Converted insurance files to typescript

--- src/pages/insurance.tsx
+++ src/pages/insurance.tsx
@@ -3,18 +3,19 @@ import { DashboardLayout } from "../components/dashboard-layout";
 import { InsuranceClaims } from "../components/insurance/insurance-claims";
 import { ref, getDatabase } from "firebase/database";
 import { useObjectVal } from "react-firebase-hooks/database";
-import { firebase } from "../firebase/clientApp";
+import { firebaseApp } from "../firebase/clientApp";
 import { InsuranceBoard } from "../components/insurance/insurance-board";
 import { InsuranceUrl } from "../firebase/databaseLinks";
 import { DashboardContainer } from "../components/dashboard-container";
+import { insuranceDb } from "../hooks/insurance";
 
 export const Insurance = () => {
-  const database = getDatabase(firebase);
+  const database = getDatabase(firebaseApp);
 
-  const [snapshot, loading, error] = useObjectVal(ref(database, InsuranceUrl()));
+  const [snapshot, loading, error] = useObjectVal<insuranceDb>(ref(database, InsuranceUrl()));
 
   return (
-    <DashboardContainer title={Insurance}>
+    <DashboardContainer title={"Insurance"}>
       {!loading && snapshot && <InsuranceBoard insurance={snapshot} />}
       <Box sx={{ mt: 3 }}>{snapshot && <InsuranceClaims claims={snapshot.claims} />}</Box>
     </DashboardContainer>
