671ce49ab82a369cd911214004edcd1d2d67b146
Howdy <howdy1078@gmail.com>
2023-01-12 22:49:55 -0700

    Switched to base hook

--- src/pages/insurance.tsx
+++ src/pages/insurance.tsx
@@ -1,18 +1,14 @@
 import { Box } from "@mui/material";
 import { DashboardLayout } from "../components/dashboard-layout";
 import { InsuranceClaims } from "../components/insurance/insurance-claims";
-import { ref, getDatabase } from "firebase/database";
-import { useObjectVal } from "react-firebase-hooks/database";
-import { firebaseApp } from "../firebase/clientApp";
 import { InsuranceBoard } from "../components/insurance/insurance-board";
-import { InsuranceUrl } from "../firebase/databaseLinks";
+import { InsuranceUrl } from "../firebase/databaseConstants";
 import { DashboardContainer } from "../components/dashboard-container";
 import { insuranceDb } from "../hooks/insurance";
+import { GetFromDatabase } from "../hooks/baseHook";
 
 export const Insurance = () => {
-  const database = getDatabase(firebaseApp);
-
-  const [snapshot, loading, error] = useObjectVal<insuranceDb>(ref(database, InsuranceUrl()));
+  const [snapshot, loading, error] = GetFromDatabase<insuranceDb>(InsuranceUrl);
 
   return (
     <DashboardContainer title={"Insurance"}>
