428076ae6e1eac03ac85085c3a3e7b0a3cf9e3b1
Howdy <howdy1078@gmail.com>
2023-01-13 22:48:09 -0700

    Updates insurance api to typescript

--- src/api/insurance-api.ts
+++ src/api/insurance-api.ts
@@ -1,17 +1,17 @@
 import { InsuranceClaimsUrl } from "../firebase/databaseConstants";
 import { createListResource, deleteListResource, updateListResource } from "./rest-list-api";
 
-export function createInsuranceClaim(baseUrl, claim) {
+export function createInsuranceClaim(baseUrl: string, claim: any) {
   const Url = baseUrl + InsuranceClaimsUrl;
   return createListResource(Url, claim);
 }
 
-export function updateInsuranceClaim(baseUrl, claim, claimId) {
+export function updateInsuranceClaim(baseUrl: string, claim: any, claimId: any) {
   const Url = baseUrl + InsuranceClaimsUrl;
   return updateListResource(Url, claim, claimId);
 }
 
-export function deleteInsuranceClaim(baseUrl, claimId) {
+export function deleteInsuranceClaim(baseUrl: string, claimId: any) {
   const Url = baseUrl + InsuranceClaimsUrl;
   return deleteListResource(Url, claimId);
 }
