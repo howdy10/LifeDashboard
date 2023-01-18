1497c15844cf6bbc21b7b2677e5cb050f12b5a07
Howdy <howdy1078@gmail.com>
2023-01-17 22:01:47 -0700

    Removed unised constant

--- src/firebase/databaseConstants.ts
+++ src/firebase/databaseConstants.ts
@@ -3,6 +3,5 @@ export const EmergencyBucketUrl = "/Savings/emergencyFund";
 export const BucketsUrl = "/Savings/buckets";
 export const SavingsUrl = "/Savings";
 export const SavingsTransactionsUrl = "/Savings/transactions";
-export const HsaTransactionsUrl = "/hsa/transactions";
 export const BudgetUrl = "/Budget";
 export const RemindersUrl = "/Reminders";
